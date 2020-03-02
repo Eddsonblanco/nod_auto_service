9a7587f73cf98baf2127cadb4e137c005972b2cf
~xavi~ <xavivzla.g@gmail.com>
2020-03-02 18:04:24 -0500

    [ADD] - layauts - routers and admin

---
+++ src/containers/views/Admin/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = (props) => {
+  console.log('===> XAVI <===: Dashboard -> props', props)
+
+  return (
+    <div>
+        Dashboard
+    </div>
+  )
+}
+
+export default Dashboard
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,58 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import loadable from '@loadable/component'
 
 import Loading from 'components/Common/Loading'
 
-const Dashboard = loadable(() => import('../containers/views/Dashboard'), {
+// layouts
+
+const MainAdmin = loadable(() => import('../containers/layouts/MainAdmin'), {
   fallback: <Loading />
 })
 
-const Home = loadable(() => import('../containers/views/Home'), {
+const Main = loadable(() => import('../containers/layouts/Main'), {
+  fallback: <Loading />
+})
+
+const Dashboard = loadable(() => import('../containers/views/Admin/Dashboard'), {
+  fallback: <Loading />
+})
+
+const Home = loadable(() => import('../containers/views/Main/Home'), {
   fallback: <Loading />
 })
 
 export default history => {
-  const pathUrl = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '/'
+  // const pathUrl = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : '/'
 
   return (
     <ConnectedRouter history={history}>
-      <Dashboard history={history}>
+      {/* <Dashboard history={history}>
         <Route component={Home} exact path={pathUrl} />
-      </Dashboard>
+      </Dashboard> */}
+
+      <Switch>
+        {/* <Route component={Login} path='/login' /> */}
+
+        <Route exact path='/admin/:path?'>
+          <MainAdmin>
+            <Switch>
+              <Route component={Dashboard} exact path='/admin/dashboard' />
+              {/* <Route component={Setting} path='/admin/setting' /> */}
+            </Switch>
+          </MainAdmin>
+        </Route>
+
+        <Route>
+          <Main>
+            <Switch>
+              <Route component={Home} exact path='/' />
+            </Switch>
+          </Main>
+        </Route>
+
+      </Switch>
     </ConnectedRouter>
   )
 }
