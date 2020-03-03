e44f9964aa60bdc37ba1076b3f93022618ca108a
·Xavi· ~ <xavivzla.g@gmail.com>
2020-03-02 22:40:40 -0500

    [UPDATE] - fix style

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,47 +1,35 @@
 import React from 'react'
-import { makeStyles } from "@material-ui/styles";
-import { CssBaseline, Button, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/styles'
+import { CssBaseline, Button, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles({
   header: {
-    width: '100%',
     background: 'gray',
+    width     : '100%'
   },
   headerContainer: {
-    background: 'orange',
-    display: 'flex',
-    margin: ' 0 auto',
     '& > div': {
       width: '100%'
     },
-    height: 100,
-    width: '90%',
+    background: 'orange',
+    display   : 'flex',
+    height    : 100,
+    margin    : ' 0 auto',
+    width     : '90%'
   },
 
   headerLogoContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      background: 'white',
-      height: '100%',
-      justifyContent: 'flex-start',
-      width: '25%'
-      margin
-    
+    alignItems    : 'center',
+    background    : 'white',
+    display       : 'flex',
+    height        : '100%',
+    justifyContent: 'flex-start',
+    width         : '25%'
+    // margin
+
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-});
+})
 
 const Header = () => {
   const classes = useStyles()
@@ -52,18 +40,18 @@ const Header = () => {
       <div className={classes.headerContainer}>
         <div className={classes.headerLeft}>
           <div className={classes.headerLogoContainer}>
-            <img className={classes.headerLogo} src=""  alt="logo" />
+            <img alt='logo' className={classes.headerLogo}  src='' />
           </div>
         </div>
         <div className={classes.headerRight}>
           <Typography>Phone</Typography>
-          <Button color="primary" variant="contained">
+          <Button color='primary' variant='contained'>
             action
           </Button>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
 export default Header
