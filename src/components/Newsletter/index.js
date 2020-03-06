04ae69af19b0772d37308af0d06a1ad119c33a39
~xavi~ <xavivzla.g@gmail.com>
2020-03-06 12:17:08 -0500

    [UPDATE] - add fonts and theme

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -8,8 +8,8 @@ import {
   InputAdornment,
   FormControl,
   Button,
-  OutlinedInput
-
+  OutlinedInput,
+  Grid
 } from '@material-ui/core'
 
 import SendIcon from '@material-ui/icons/Send'
@@ -26,39 +26,42 @@ const Newsletter = () => {
   const classes = useStyles()
 
   return (
-    <Container>
-      <Typography>
-        Subscribe to our newspaper
-      </Typography>
-      <Typography>
-        Our service team is available 7 days a week:
-      </Typography>
-      <Typography>
-        Monday - Friday 6 AM to 5 PM PSTSaturday - Sunday 7 AM - 4 PM PST
-      </Typography>
+    <Container maxWidth='lg'>
+      <Grid
+        alignItems='center'
+        container
+        direction='column'
+        justify='flex-start'
+        spacing={3}>
+        <Typography variant='h5'>Subscribe to our newspaper</Typography>
+        <Typography variant='subtitle1'>Our service team is available 7 days a week:</Typography>
+        <Typography variant='subtitle1'>
+            Monday - Friday 6 AM to 5 PM PSTSaturday - Sunday 7 AM - 4 PM PST
+        </Typography>
 
-      <FormControl /* className={clsx(classes.margin, classes.textField)} */ variant='outlined'>
-        <OutlinedInput
-          endAdornment={
-            <InputAdornment position='end'>
-              <SendIcon />
-            </InputAdornment>
-          }
-          id='outlined-adornment-password'
-          labelWidth={0}
-          placeholder='domain@domain.com'
-          // onChange={handleChange('password')}
-          value='' />
-      </FormControl>
+        <FormControl /* className={clsx(classes.margin, classes.textField)} */ variant='outlined'>
+          <OutlinedInput
+            endAdornment={
+              <InputAdornment position='end'>
+                <SendIcon />
+              </InputAdornment>
+            }
+            id='outlined-adornment-password'
+            labelWidth={0}
+            placeholder='domain@domain.com'
+            // onChange={handleChange('password')}
+            value='' />
+        </FormControl>
 
-      <div className={classes.actions}>
-        <Button color='primary' variant='contained'>
-          Primary
-        </Button>
-        <Button color='primary' variant='contained'>
-          Primary
-        </Button>
-      </div>
+        <div className={classes.actions}>
+          <Button color='primary' variant='contained'>
+              Primary
+          </Button>
+          <Button color='secondary' variant='contained'>
+              Secondary
+          </Button>
+        </div>
+      </Grid>
 
     </Container>
   )
