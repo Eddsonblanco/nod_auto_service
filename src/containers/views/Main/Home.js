e54557a56f27804624f6dc224fd737a00c590282
·Xavi· ~ <xavivzla.g@gmail.com>
2020-03-08 00:50:27 -0500

    [UPDATE] - create carousel

--- src/containers/views/Main/Home.js
+++ src/containers/views/Main/Home.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
+import Carousel from 'components/Common/Carousel'
+
 import { makeStyles } from '@material-ui/styles'
 import {
   Container,
-  Typography,
-  Grid
+  Typography
+  // Grid
 } from '@material-ui/core'
 
 // import Companies from 'components/Companies'
 import Banner from 'components/Banner/Banner'
-import CardService from 'components/CardService'
+// import CardService from 'components/CardService'
 import Testimony from 'components/Testimony'
 
 const useStyles = makeStyles({
@@ -61,8 +63,103 @@ export default function Home() {
           variant='h5'>
           What people say about us
         </Typography>
+        <Carousel settings={
+          9 <= 3 ? {
+            responsive: [
+              {
+                breakpoint: 5000,
+                settings  : {
+                  infinite      : 5 > 4,
+                  slidesToScroll: 4,
+                  slidesToShow  : 4
+                }
+              },
+              {
+                breakpoint: 1024,
+                settings  : {
+                  infinite      : 4 > 3,
+                  slidesToScroll: 3,
+                  slidesToShow  : 3
+                }
+              },
+              {
+                breakpoint: 920,
+                settings  : {
+                  infinite      : 3 > 2,
+                  slidesToScroll: 2,
+                  slidesToShow  : 2
+                }
+              },
+              {
+                breakpoint: 520,
+                infinite  : 2 > 1,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 1
+                }
+              }
+            ]
+          } : {
+            infinite  : true,
+            responsive: [
+              {
+                breakpoint: 5000,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 3
+                }
+              },
+              {
+                breakpoint: 1200,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 2
+                }
+              },
+              {
+                breakpoint: 1024,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 2
+                }
+              },
+              {
+                breakpoint: 920,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 2
+                }
+              },
+              {
+                breakpoint: 520,
+                settings  : {
+                  slidesToScroll: 1,
+                  slidesToShow  : 1
+                }
+              }
+            ],
+            rows: 1
+          }
+        }>
+          {
+            [ 1, 2, 3, 4, 5, 6 ].map((job, index) => (
+              <div
+                key={`slide-${index}`}>
+                <Testimony
+                  key={`slide-${index}`} />
 
-        <Testimony />
+                {/* <CardJob
+                history={history}
+                job={job}
+                key={`slide-${index}`}
+                type='home'
+                userLogin={userLogin}
+                wished={wishList.includes(job.job_id)} /> */}
+
+              </div>
+            ))
+          }
+        </Carousel>
       </Container>
 
     </div>
