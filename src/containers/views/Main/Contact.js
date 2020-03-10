590ae70c2915b4af105f1a67d8a997d364e91b9a
Xavi <xavivzla.g@gmail.com>
2020-03-10 07:17:13 -0500

    [ADD] -struture view contact

--- src/containers/views/Main/Contact.js
+++ src/containers/views/Main/Contact.js
@@ -1,44 +1,122 @@
 import React from 'react'
 
-// import { makeStyles } from '@material-ui/styles'
+import {
+  Grid,
+  Container,
+  Typography,
+  TextField,
+  Button
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
-// const useStyles = makeStyles({
-//   '@keyframes appLogoSpin': {
-//     from: {
-//       transform: 'rotate(0deg)'
-//     },
-//     to: {
-//       transform: 'rotate(360deg)'
-//     }
-//   },
-//   appContainer: {
-//     textAlign: 'center'
-//   },
-//   appHeader: {
-//     alignItems     : 'center',
-//     backgroundColor: '#282c34',
-//     color          : 'white',
-//     display        : 'flex',
-//     flexDirection  : 'column',
-//     fontSize       : 'calc(10px + 2vmin)',
-//     justifyContent : 'center',
-//     minHeight      : '100vh'
-//   },
-//   appLink: {
-//     color: '#61dafb'
-//   },
-//   appLogo: {
-//     animation: '$appLogoSpin infinite 20s linear',
-//     height   : '40vmin'
-//   }
-// })
+const useStyles = makeStyles({
+  carContainer: {
+    display: 'flex'
+  },
+  carContainerText: {
+    marginLeft: 14
+  }
+})
 
 export default function Contact() {
-  // const classes = useStyles()
+  const classes = useStyles()
 
   return (
-    <div>
-      Contact
-    </div>
+    <Container maxWidth='lg'>
+      <Grid container>
+        <Grid item xs='5'>
+          <div>
+            <Typography variant='h4'>Contact.</Typography>
+            <Typography variant='body1'>Leave us a message</Typography>
+
+            <Grid container spacing={4}>
+              <Grid item xs='6'>
+                <TextField
+                  helperText='Full width!'
+                  id='standard-full-width'
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  label='Label'
+                  margin='normal'
+                  placeholder='Placeholder'
+                  style={{ margin: 8 }} />
+              </Grid>
+              <Grid item xs='6'>
+                <TextField
+                  helperText='Full width!'
+                  id='standard-full-width'
+                  InputLabelProps={{
+                    shrink: true
+                  }}
+                  label='Label'
+                  margin='normal'
+                  placeholder='Placeholder'
+                  style={{ margin: 8 }} />
+              </Grid>
+            </Grid>
+
+            <TextField
+              fullWidth
+              helperText='Full width!'
+              id='standard-full-width'
+              InputLabelProps={{
+                shrink: true
+              }}
+              label='Label'
+              margin='normal'
+              placeholder='Placeholder'
+              style={{ margin: 8 }} />
+
+            <TextField
+              fullWidth
+              helperText='Full width!'
+              id='standard-full-width'
+              InputLabelProps={{
+                shrink: true
+              }}
+              label='Label'
+              margin='normal'
+              placeholder='Placeholder'
+              style={{ margin: 8 }} />
+
+            <Button color='primary' variant='contained'>I want you to contact me</Button>
+          </div>
+        </Grid>
+        <Grid item xs='7'>
+          <img src='https://cdn.zeplin.io/5dc2fe76c82d4954cfd1d481/assets/8d889a0c-1412-48e8-a566-cc3a2c71d8b5.png' />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={5}>
+        <Grid item xs='4'>
+          <div className={classes.carContainer}>
+            <img src='https://cdn.zeplin.io/5dc2fe76c82d4954cfd1d481/assets/7e63f71f-4359-4307-b9c6-b0bc6286b576.svg' />
+            <div className={classes.carContainerText}>
+              <Typography>Location</Typography>
+              <Typography>130 Industrial Way, Brisbane, CA 94005</Typography>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs='4'>
+          <div className={classes.carContainer}>
+            <img src='https://cdn.zeplin.io/5dc2fe76c82d4954cfd1d481/assets/7e63f71f-4359-4307-b9c6-b0bc6286b576.svg' />
+            <div className={classes.carContainerText}>
+              <Typography>Location</Typography>
+              <Typography>130 Industrial Way, Brisbane, CA 94005</Typography>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs='4'>
+          <div className={classes.carContainer}>
+            <img src='https://cdn.zeplin.io/5dc2fe76c82d4954cfd1d481/assets/7e63f71f-4359-4307-b9c6-b0bc6286b576.svg' />
+            <div className={classes.carContainerText}>
+              <Typography>Location</Typography>
+              <Typography>130 Industrial Way, Brisbane, CA 94005</Typography>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
