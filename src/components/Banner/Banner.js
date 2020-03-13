53dc2098a623d2552a2dca4923f686b59cf36b09
Xavi <xavivzla.g@gmail.com>
2020-03-13 12:56:30 -0500

    [UPDATE] - change banner style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,17 +4,17 @@ import { Typography, Button, Container } from '@material-ui/core'
 
 const useStyles = makeStyles(theme =>({
 
-  CallToAction: {
-    '&:hover': {
-      backgroundColor: theme.palette.primary.main
-    },
-    color        : theme.palette.common.white,
-    fontWeight   : 300,
-    paddingBottom: 6,
-    paddingTop   : 6
-  },
+  // CallToAction: {
+  //   '&:hover': {
+  //     backgroundColor: theme.palette.primary.main
+  //   },
+  //   color        : theme.palette.common.white,
+  //   fontWeight   : 300,
+  //   paddingBottom: 6,
+  //   paddingTop   : 6
+  // },
   backgroundImg: {
-    backgroundRepeat: 'noRepeat',
+    backgroundRepeat: 'no-repeat',
     backgroundSize  : 'contain',
     height          : '100%',
     left            : 0,
@@ -23,16 +23,28 @@ const useStyles = makeStyles(theme =>({
     width           : '100%'
   },
   containerBanner: {
-    height: '100%'
+    alignItems: 'center',
+    display   : 'flex',
+    height    : '100%'
   },
-  contentBanner: {
-    fontSize    : '0.9rem',
-    marginBottom: 30
+  overlay: {
+    backgroundImage: 'linear-gradient(to bottom, rgba(53, 53, 53, 0.23) 3%, rgba(246, 78, 78, 0.99))',
+    height         : '100%',
+    left           : 0,
+    opacity        : '0.32',
+    position       : 'absolute',
+    top            : 0,
+    width          : '100%'
   },
+  // contentBanner: {
+  //   fontSize    : '0.9rem',
+  //   marginBottom: 30
+  // },
   titleBanner: {
-    fontSize    : '2.3rem',
+    color       : theme.palette.common.white,
+    fontSize    : '2.5rem',
     fontWeight  : '900',
-    lineHeight  : '4rem',
+    lineHeight  : '60px',
     marginBottom: 30
 
   },
@@ -43,14 +55,15 @@ const useStyles = makeStyles(theme =>({
     position  : 'relative'
   },
   wrapperTextBanner: {
-    alignContent  : 'center',
-    color         : theme.palette.common.white,
-    display       : 'flex',
-    flexDirection : 'column',
-    height        : '100%',
-    justifyContent: 'center',
-    position      : 'relative',
-    width         : '50%'
+    // alignContent  : 'center',
+    color   : theme.palette.common.white,
+    // display       : 'flex',
+    // flexDirection : 'column',
+    // height        : '100%',
+    // justifyContent: 'center',
+    maxWidth: 640,
+    position: 'relative',
+    width   : '100%'
   }
 
 }))
@@ -63,12 +76,13 @@ export default function banner() {
     <div className={classes.wrapperBanner}>
       <Container className={classes.containerBanner} maxWidth='lg'>
         <img className={classes.backgroundImg} src='https://cdn.zeplin.io/5dc2fe76c82d4954cfd1d481/assets/07a463cc-2270-4ed1-a8ef-cc0a75448fc7.png'></img>
+        <div className={classes.overlay} />
         <div className={classes.wrapperTextBanner}>
           <Typography className={classes.titleBanner}> Energistically morph open-source technologies
             rather than transparent technologies </Typography>
           <Typography className={classes.contentBanner}> Competently maintain holistic internal or "organic" sources via compelling benefits.
             Collaboratively engage error-free paradigms rather than. </Typography>
-          <Button className={classes.CallToAction} variant='outlined'> GET AN APPOIMENT NOW </Button>
+          <Button className={classes.CallToAction} color='inherit' variant='outlined'> GET AN APPOIMENT NOW </Button>
         </div>
       </Container>
     </div>
